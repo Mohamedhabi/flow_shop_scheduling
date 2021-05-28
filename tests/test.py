e9f018078748b4be80b72aa9735722990b4135f6
Habi Mohamed <hm_habi@esi.dz>
2021-05-28 21:07:48 +0100

    initial ACO params

--- tests/test.py
+++ tests/test.py
@@ -1,23 +1,18 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from numpy.core.numeric import _rollaxis_dispatcher
 from utils import Instance, Benchmark
 from fsp import ACO, specific_heuristic_NEH,branch_and_bound, meta_heuristic_ga
 import numpy as np
 import json
 import re
-from fsp.branch_and_bound import evaluateSeqeunce
+
+
 OUTPUT_FOLDER = 'results'
 
 tai_benchmarks = [
-    (20,5),
-    (20,10),
     (20,20),
-    (50,5),
-    (50,10),
-    (50,20),
-    (100,5),
-    (100,10),
-    (100,20),
-    (500,20),
+
     ]
 
 def get_result_file_name(method,jobs_number,machines_number):
@@ -32,16 +27,24 @@ def get_normalize_json(results):
     return  re.sub(r'"\[', '[', tmp)
 
 def test_ACO():
-    for b in tai_benchmarks:
-        benchmark = Benchmark(b[0], b[1], benchmark_folder = './benchmarks')
-        instances_number = benchmark.get_instances_number()
-        results = []
-        for nb in range(1):
-            instance = benchmark.get_instance(nb)
-            results.append({
-                'instance': nb,
-                'results': ACO.get_results(instance)
-            })
+    with open('tests/params/aco.json') as f:
+        data = json.load(f)
+        print(type(data[0]))
+    for params in data:
+        for b in tai_benchmarks:
+            benchmark = Benchmark(b[0], b[1], benchmark_folder = './benchmarks')
+            instances_number = benchmark.get_instances_number()
+            results = {
+                'params': params,
+                'results': []
+            }
+            for nb in range(1):
+                instance = benchmark.get_instance(nb)
+                results['results'].append({
+                    'instance': nb,
+                    'results': ACO.get_results(instance, **params)
+                })
+
         with open(get_result_file_name('aco', b[0], b[1]), 'w+') as f:
             json.dump(results , f, indent = 2)
 
@@ -74,9 +77,10 @@ def test_ga():
         with open(get_result_file_name('ga', b[0], b[1]), 'w+') as f:
             f.write(get_normalize_json(results))
 
+if __name__ == '__main__':
 #test_ga()
-benchmark = Benchmark(20,20, benchmark_folder = './benchmarks')
-instance = benchmark.get_instance(0)
-print([13, 2, 19, 8, 4, 11, 9, 10, 1, 5, 17, 14, 6, 12, 7, 15, 3, 16, 0, 18])
-print(instance.makespan([13, 2, 19, 8, 4, 11, 9, 10, 1, 5, 17, 14, 6, 12, 7, 15, 3, 16, 0, 18]))
-print(evaluateSeqeunce(instance,tuple([13, 2, 19, 8, 4, 11, 9, 10, 1, 5, 17, 14, 6, 12, 7, 15, 3, 16, 0, 18])))
+    test_ACO()
+    benchmark = Benchmark(20,20, benchmark_folder = './benchmarks')
+    instance = benchmark.get_instance(0)
+    print([16, 0, 1, 18, 14, 12, 3, 8, 6, 4, 13, 2, 19, 15, 11, 17, 10, 7, 9, 5])
+    print(instance.makespan([16, 0, 1, 18, 14, 12, 3, 8, 6, 4, 13, 2, 19, 15, 11, 17, 10, 7, 9, 5]))
