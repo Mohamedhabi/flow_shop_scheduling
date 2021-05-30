de769ac95ee77df1543cb46e75cc075cc32b20a9
Mohamedhabi <hm_habi@esi.dz>
2021-05-30 03:55:12 +0100

    FIX: CDS  get_results

--- fsp/CDS.py
+++ fsp/CDS.py
@@ -4,20 +4,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import *
 import numpy as np
 from .branch_and_bound import *
-def get_results(bench: Benchmark):
-    """Get the results of the algorithm on the specified instance
+import time
 
-    Args:
-        instance (class:instance): an FPS instance
-
-    Returns:
-        list: jobs order
-    """
-
-    return {
-        "C_max": 13,
-        "order":list(range(instance.get_jobs_number()))
-        }
 def get_cds_sub_sequences(instance: Instance):
     """an algorithem that create (m-1) 2-machines PFS instances from m-machine FPS instance
 
@@ -30,42 +18,43 @@ def get_cds_sub_sequences(instance: Instance):
             "sequences" : an array of nb_sequences FPS instances created
         }
     """
-    m = instance.get_machines_number(); 
-    n = instance.get_jobs_number(); 
-    sequences = []; 
+    m = instance.get_machines_number()
+    n = instance.get_jobs_number()
+    sequences = []
     for i in range(m-1):
-        instance_two_machines = np.zeros((n,2)); 
+        instance_two_machines = np.zeros((n,2))
         for j in range(i+1):
-            instance_two_machines[:,0] += instance.get_machine_costs(j); 
-            instance_two_machines[:,1] += instance.get_machine_costs(m-j-1); 
-        sequences.append(Instance(instance_two_machines)); 
+            instance_two_machines[:,0] += instance.get_machine_costs(j)
+            instance_two_machines[:,1] += instance.get_machine_costs(m-j-1)
+        sequences.append(Instance(instance_two_machines))
     return {
         "nb_sequences" : m-1,
         "sequences" : sequences
     }
+
 def makespan(instance: Instance, schedule: list): 
-    C_max = 0; # makespan 
-    n = instance.get_jobs_number(); # number of jobs in the instance 
-    m = instance.get_machines_number(); # number of machines 
-    completionTimes = np.zeros((n,m)); #an (n,m) array that gives the completion time for a job i in machine j 
+    C_max = 0 # makespan 
+    n = instance.get_jobs_number() # number of jobs in the instance 
+    m = instance.get_machines_number() # number of machines 
+    completionTimes = np.zeros((n,m)) #an (n,m) array that gives the completion time for a job i in machine j 
     #we iterate over the schedule list, foreach job, we calculate its completion time on all machines 
     for i in range(len(schedule)):
-        jobCosts = instance.get_job_costs(schedule[i]); # an array of all operations' time for job i 
+        jobCosts = instance.get_job_costs(schedule[i]) # an array of all operations' time for job i 
         if(i==0):
-            completionTimes[schedule[i],:] = jobCosts; 
+            completionTimes[schedule[i],:] = jobCosts
         else:
             for j in range(m):
                 if(j == 0): #case when we are in the first operation for th i'th job 
-                    completionTimes[schedule[i],j] = completionTimes[schedule[i-1],j] +jobCosts[j]; 
+                    completionTimes[schedule[i],j] = completionTimes[schedule[i-1],j] +jobCosts[j]
                 else:
                     if(completionTimes[schedule[i],j-1] > completionTimes[schedule[i-1],j]):
-                        completionTimes[schedule[i],j] = completionTimes[schedule[i],j-1]+jobCosts[j]; 
+                        completionTimes[schedule[i],j] = completionTimes[schedule[i],j-1]+jobCosts[j] 
                     else: 
-                        completionTimes[schedule[i],j] = completionTimes[schedule[i-1],j]+jobCosts[j]; 
-    C_max = completionTimes[schedule[len(schedule)-1],m-1]; 
+                        completionTimes[schedule[i],j] = completionTimes[schedule[i-1],j]+jobCosts[j] 
+    C_max = completionTimes[schedule[len(schedule)-1],m-1]
     return C_max; 
 
-def cds(instance: Instance):
+def get_results(instance: Instance):
     """an Optimal O(m*nlogn) algorithm for solving the PFSP (n jobs, m machines) based on jhonson's algorithem
 
     Args:
@@ -77,25 +66,27 @@ def cds(instance: Instance):
             "order" : the order of jobs scheduled on the machines
         }
     """
+    start = time.perf_counter()
     #creation of (m-1) virtual 2-machines PFS problem: 
-    output = get_cds_sub_sequences(instance); 
-    m = output['nb_sequences'] + 1; 
-    sequences = output['sequences']; 
-    C_max = 0;  
-    order = []; 
+    output = get_cds_sub_sequences(instance)
+    m = output['nb_sequences'] + 1
+    sequences = output['sequences'] 
+    C_max = 0
+    order = []
     for i in range(m-1): 
         #calculate the order using jhonson algorithem: 
-        schedule = johnson(sequences[i]); 
-        o = schedule['order']; 
-        c = makespan(instance,o); 
+        schedule = johnson(sequences[i])
+        o = schedule['order']
+        c = makespan(instance,o) 
         if(i==0):
-            C_max = c; 
-            order = o; 
+            C_max = c 
+            order = o 
         else:
-            if(c<C_max):
-                C_max = c; 
-                order = o; 
+            if(c < C_max):
+                C_max = c 
+                order = o 
     return {
-        "C_max": C_max,
-        "order": order
-    } 
+        "C_max" :  C_max,
+        "order" : order,
+        "time" : time.perf_counter() - start,
+    }
